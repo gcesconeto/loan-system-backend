7490bbf7ed59422d2f3e73c16c0bd5c8f5c834ef
Guilherme Cesconeto <cesconeto@gmail.com>
2022-05-06 11:31:38 -0300

    refactor: remove some unused lines

--- src/services/ledger/balance.js
+++ src/services/ledger/balance.js
@@ -4,6 +4,5 @@ const { getBalance } = require('../helpers');
 module.exports = async () => {
     const ledgerList = await ledger.findAll();
     const balance = getBalance(ledgerList);
-    console.log(balance);
     return balance;
 };

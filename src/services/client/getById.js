e1d8380ffa51819606a567ad418bbdd6e54df986
Guilherme Cesconeto <cesconeto@gmail.com>
2022-05-23 10:19:57 -0300

    fix: throws error if user not found

--- src/services/client/getById.js
+++ src/services/client/getById.js
@@ -1,6 +1,8 @@
 const { client } = require('../../database/models');
+const err = require('../../errors/errors');
 
 module.exports = async (clientId) => {
   const foundClient = await client.findByPk(clientId);
+  if (!foundClient) throw err.CLIENT_NOT_FOUND;
   return foundClient;
 };

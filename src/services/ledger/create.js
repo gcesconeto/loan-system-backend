bea696b9004c6c173d5d6fd0812ed754ad484806
Guilherme Cesconeto <cesconeto@gmail.com>
2022-05-23 09:21:58 -0300

    fix: entry validation was rejecting valid new ledger input

--- src/services/ledger/create.js
+++ src/services/ledger/create.js
@@ -4,7 +4,7 @@ const err = require('../../errors/errors');
 module.exports = async ({ loanId, settlementId, date, amount }) => {
     const loanFound = await loan.findByPk(loanId);
     const settlementFound = await settlement.findByPk(settlementId);
-    if (!loanFound && !settlementFound) throw err.NOT_FOUND;
+    if ((loanId && !loanFound) || (settlementId && !settlementFound)) throw err.NOT_FOUND;
     const createdEntry = await ledger.create(
         { loanId, settlementId, date, amount },
     );

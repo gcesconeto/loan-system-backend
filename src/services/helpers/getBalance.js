cc4e7605c83647d2d3f00233b92a5b28363cdb11
Guilherme Cesconeto <cesconeto@gmail.com>
2022-06-07 14:14:52 -0300

    feat: add credit and debit along balance report

--- src/services/helpers/getBalance.js
+++ src/services/helpers/getBalance.js
@@ -2,7 +2,6 @@ module.exports = (ledger) => {
   const processedLedger = ledger.map((entry) => entry.toJSON());
   let credit = 0;
   let debit = 0;
-  // const balance = processedLedger.reduce((sum, entry) => sum + entry.amount, 0);
   processedLedger.forEach((entry) => {
     if (entry.amount > 0) {
       credit += entry.amount;
@@ -11,5 +10,5 @@ module.exports = (ledger) => {
     }
   });
   const balance = credit + debit;
-  return balance;
+  return { balance, credit, debit };
 };

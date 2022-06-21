46dba631e0d46e22c8c088e30e64c473a8297d7a
Guilherme Cesconeto <cesconeto@gmail.com>
2022-06-21 12:57:24 -0300

    refactor: move from mySQL to postgres

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,24 +2,21 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME,
-    host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
   },
-  test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME,
-    host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME,
-    host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+    ssl: {
+       require: true,
+       rejectUnauthorized: false
+     }
+   }
   },
 };
